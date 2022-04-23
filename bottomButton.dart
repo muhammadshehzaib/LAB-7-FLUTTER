afef5cb9dbb0245ee8dffc85d894bf1e496060d4
shehzaib <shehzaib222@gmail.com>
2022-04-23 23:34:18 +0500

    Lab 7

---
+++ bottomButton.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class BottomButton extends StatelessWidget {
+  const BottomButton({this.text});
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Color.fromARGB(255, 189, 47, 94),
+          borderRadius: BorderRadius.circular(10)),
+      height: 40,
+      width: double.infinity,
+      child: Center(child: Text('$text')),
+    );
+  }
+}
---
+++ constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kActiveColor = Color.fromARGB(255, 42, 48, 179);
+const kInactiveColor = Color(0xFF111328);
